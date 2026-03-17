1d4e519a5bfb44b364c9825252c48ec0152b2d72
Sebastián Giménez <sebastiangimenezomar@gmail.com>
2026-03-17 11:47:44 -0300

    Fix base URL for local dev, add @types/node

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sebastianomargimenez.github.io',
-  base: '/Mendoza-LENIS-GSAP',
+  base: process.env.GITHUB_ACTIONS ? '/Mendoza-LENIS-GSAP' : '/',
   server: {
     host: true,
   },
